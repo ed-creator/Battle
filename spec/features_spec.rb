b01d3ef613d33843351c7448ef1359329b39bbe4
Edward Ward <edwardwardward@gmail.com>
2017-03-28 16:41:32 +0100

    Add view HP

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -8,3 +8,13 @@
         expect(page).to have_content 'Bob vs. Jared'
     end
   end
+
+  feature 'View Hit Points' do
+    scenario 'Player 1 can see Player 2 Hit Points' do
+      visit('/')
+      fill_in :player1, with: "Bob"
+      fill_in :player2, with: "Jared"
+      click_button 'Submit'
+      expect(page).to have_content('Jared: 10HP')
+    end
+  end
